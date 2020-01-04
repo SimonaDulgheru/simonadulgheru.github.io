47c5eaba72ec3bbce751cce730e950eb11698e1d
Simona <s.c.dulgheru@gmail.com>
2020-01-04 18:40:05 +0000

    desktop and tablet version updated

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const burgerNavBar = document.querySelector(".nav-links");
+
 function navBar() {
-    const burgerNavBar = document.querySelector(".nav-links");
+    
     if (burgerNavBar.style.display === "block") {
         burgerNavBar.style.display = "none";
     } else {
         burgerNavBar.style.display = "block";
     }
   }
+  navBar();
